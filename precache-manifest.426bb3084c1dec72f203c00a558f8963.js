7d2133455571450ff018c8a48c471d45f5d6549b
roma-garbuz <roma-garbuz@ukr.net>
2020-04-16 23:05:46 +0300

    Updates

---
+++ precache-manifest.426bb3084c1dec72f203c00a558f8963.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca162037e9bed1a4f21d5d6ea6b0c0e5",
+    "url": "/sklad1/index.html"
+  },
+  {
+    "revision": "9e800ef38d29fbed1db8",
+    "url": "/sklad1/static/css/main.8311d667.chunk.css"
+  },
+  {
+    "revision": "9e812e8ca9bd8355c699",
+    "url": "/sklad1/static/js/2.0274f38a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sklad1/static/js/2.0274f38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40a03f69424848791d7",
+    "url": "/sklad1/static/js/3.284afc1c.chunk.js"
+  },
+  {
+    "revision": "43d56270af91ebe214b1",
+    "url": "/sklad1/static/js/4.271830b5.chunk.js"
+  },
+  {
+    "revision": "9e800ef38d29fbed1db8",
+    "url": "/sklad1/static/js/main.a9546e43.chunk.js"
+  },
+  {
+    "revision": "93230032cfb6c097703c",
+    "url": "/sklad1/static/js/runtime-main.3cd2754a.js"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/sklad1/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/sklad1/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/sklad1/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/sklad1/static/media/themify.e23a7dca.ttf"
+  }
+]);
